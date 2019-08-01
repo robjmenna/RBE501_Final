93d13ea2b459862f07c07e13d8cdf170aecc2147
Robert Menna <robjmenna@gmail.com>
2019-08-01 14:57:05 -0400

    Added more symbolic calculations

--- generate_torque.m
+++ generate_torque.m
@@ -2,22 +2,22 @@ function Tau = generate_torque(path_1, path_2, path_3, path_4, path_5, path_6, K
 syms q1 q2 q3 q4 q5 q6
 syms dq1 dq2 dq3 dq4 dq5 dq6
 syms ddq1 ddq2 ddq3 ddq4 ddq5 ddq6
-syms m1 m2 m3
+syms m1 m2 m4 m6 mp pi_
 syms a1 a2 a3 d1 d2 d3 g
 
 mt = 53;
-m1 = mt * 0.2869;
-m2 = mt * 0.3763;
-m4 = mt * (0.1713 + 0.1363);
-m6 = mt * (0.0283);
-mp = 0;
+% m1 = mt * 0.2869;
+% m2 = mt * 0.3763;
+% m4 = mt * (0.1713 + 0.1363);
+% m6 = mt * (0.0283);
+% mp = 0;
 
 fprintf('Starting torque controller\n');
 
 d = [d1 0 0 d2 0 d3];
-theta = [q1 q2+pi/2 q3 q4 q5 q6];
+theta = [q1 q2+pi_/2 q3 q4 q5 q6];
 a = [a1 a2 a3 0 0 0];
-alpha = [pi/2 0 pi/2 -pi/2 pi/2 0];
+alpha = [pi_/2 0 pi_/2 -pi_/2 pi_/2 0];
 
 T01 = DH(alpha(1), a(1), d(1), theta(1));
 T12 = DH(alpha(2), a(2), d(2), theta(2));
@@ -47,17 +47,17 @@ v_m4 = J_l4 * [dq1 ; dq2 ; dq3; dq4];
 v_m6 = J_l6 * [dq1 ; dq2 ; dq3; dq4; dq5; dq6];
 
 fprintf('Finding K and P\n');
-K1 = subs(0.5 * m1 * (v_m1.' * v_m1), [a1 d1], [50/2 330/2]);
-K2 = subs(0.5 * m2 * (v_m2.' * v_m2), [a1 d1 a2], [50 330 330/2]);
-K4 = subs(0.5 * m4 * (v_m4.' * v_m4), [a1 d1 a2 a3 d2], [50 330 330 35/2 335/2]);
-K6 = subs(0.5 * m6 * (v_m6.' * v_m6), [a1 d1 a2 a3 d2 d3], [50 330 330 35 335 80/2]);
-K_p = subs(0.5 * mp * (v_m6.' * v_m6), [a1 d1 a2 a3 d2 d3], [50 330 330 35 335 80]);
+K1 = subs(0.5 * m1 * (v_m1.' * v_m1), [a1 d1], [a1/2 d1/2]);
+K2 = subs(0.5 * m2 * (v_m2.' * v_m2), a2, a2/2);
+K4 = subs(0.5 * m4 * (v_m4.' * v_m4), [a3 d2], [a3/2 d2/2]);
+K6 = subs(0.5 * m6 * (v_m6.' * v_m6), d3, d3/2);
+K_p = 0.5 * mp * (v_m6.' * v_m6);
 
-P1 = subs(m1 * g * T01(3,4), [a1 d1], [50/2 330/2]);
-P2 = subs(m2 * g * T02(3,4), [a1 d1 a2], [50 330 330/2]);
-P4 = subs(m4 * g * T04(3,4), [a1 d1 a2 a3 d2], [50 330 330 35/2 335/2]);
-P6 = subs(m6 * g * T0f(3,4), [a1 d1 a2 a3 d2 d3], [50 330 330 35 335 80/2]);
-Pp = subs(mp * g * T0f(3,4), [a1 d1 a2 a3 d2 d3], [50 330 330 35 335 80]);
+P1 = subs(m1 * g * T01(3,4), [a1 d1], [a1/2 d1/2]);
+P2 = subs(m2 * g * T02(3,4), a2, a2/2);
+P4 = subs(m4 * g * T04(3,4), [a3 d2], [a3/2 d2/2]);
+P6 = subs(m6 * g * T0f(3,4), d3, d3/2);
+Pp = mp * g * T0f(3,4);
 fprintf('Finding L\n');
 K = K1 + K2 + K4 + K6 + K_p;
 P = P1 + P2 + P4 + P6 + Pp;
@@ -174,7 +174,7 @@ C41;
 C51;
 C61];
 
-fprintf('Calculating controller.');
+fprintf('Calculating controller.\n');
 last_index = size(path_1, 2);
 uno = [path_1(3,last_index); path_2(3,last_index); path_3(3,last_index);...
     path_4(3,last_index); path_5(3,last_index); path_6(3,last_index)] - transpose(ddq);
@@ -183,34 +183,19 @@ dos = [path_1(2,last_index); path_2(2,last_index); path_3(2,last_index);...
 tres = [path_1(1,last_index); path_2(1,last_index); path_3(1,last_index);...
     path_4(1,last_index); path_5(1,last_index); path_6(1,last_index)]- transpose(q);
 
-a = uno + Kv*dos + Kp*tres;
+%a = uno + Kv*dos + Kp*tres;
 Tau = zeros(6,last_index);
-quatro = M*a;
-controller = quatro+C+G;
+%quatro = M*a;
+controller = M+C+G;
 for i=1:last_index
     fprintf(['Processing torque trajectory.' num2str(i) '/' num2str(last_index) '\n']);
-    first_term = subs(quatro,[q dq ddq],...
-        [path_1(1,i) path_2(1,i) path_3(1,i) path_4(1,i) path_5(1,i) path_6(1,i)...
-        path_1(2,i) path_2(2,i) path_3(2,i) path_4(2,i) path_5(2,i) path_6(2,i)...
-        path_1(3,i) path_2(3,i) path_3(3,i) path_4(3,i) path_5(3,i) path_6(3,i)]);
-    
-    second_term = subs(C,[q dq ddq],...
-        [path_1(1,i) path_2(1,i) path_3(1,i) path_4(1,i) path_5(1,i) path_6(1,i)...
-        path_1(2,i) path_2(2,i) path_3(2,i) path_4(2,i) path_5(2,i) path_6(2,i)...
-        path_1(3,i) path_2(3,i) path_3(3,i) path_4(3,i) path_5(3,i) path_6(3,i)]);
-    
-    third_term = subs(G, [q dq ddq g],...
+
+    Tau(1:6,i) = subs(controller,...
+        [q dq ddq g a1 a2 a3 d1 d2 d3 m1 m2 m4 m6 mp pi_],...
         [path_1(1,i) path_2(1,i) path_3(1,i) path_4(1,i) path_5(1,i) path_6(1,i)...
         path_1(2,i) path_2(2,i) path_3(2,i) path_4(2,i) path_5(2,i) path_6(2,i)...
         path_1(3,i) path_2(3,i) path_3(3,i) path_4(3,i) path_5(3,i) path_6(3,i)...
-        9.8]);
-    Tau(1:6,i) = first_term + second_term + third_term;
-%     Tau(1:6,i) = subs(controller,...
-%         [q dq ddq g],...
-%         [path_1(1,i) path_2(1,i) path_3(1,i) path_4(1,i) path_5(1,i) path_6(1,i)...
-%         path_1(2,i) path_2(2,i) path_3(2,i) path_4(2,i) path_5(2,i) path_6(2,i)...
-%         path_1(3,i) path_2(3,i) path_3(3,i) path_4(3,i) path_5(3,i) path_6(3,i)...
-%         9.8]);
+        9.8 50 330 35 330 335 80 (mt*0.2869) (mt*0.3763) (mt*(0.1713 + 0.1363)) (mt*0.0283) 0 3.14]);
 end
 
 end
